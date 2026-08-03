8902ce914d9f3a757148b339d1bdc54125d705b6
Andrew Erickson <aerickson@mozilla.com>
2026-08-03 10:38:09 -0700

    fix: package start lag maintenance runner

--- worker_health/pool_classifier_web/scripts/db_maintenance.py
+++ worker_health/pool_classifier_web/scripts/db_maintenance.py
@@ -15,6 +15,7 @@ import sys
 from collections.abc import Callable
 
 from worker_health.pool_classifier_web.scripts import backfill_m007_task_timestamps
+from worker_health.pool_classifier_web.scripts import backfill_start_lag_all_pools
 from worker_health.pool_classifier_web.scripts import datastore_summary
 from worker_health.pool_classifier_web.scripts import utilization_query_plan
 from worker_health.pool_classifier_web.scripts.create_unresolved_task_run_index import (
@@ -23,7 +24,6 @@ from worker_health.pool_classifier_web.scripts.create_unresolved_task_run_index 
 from worker_health.pool_classifier_web.scripts.create_utilization_task_run_index import (
     create_utilization_task_run_index,
 )
-from scripts import backfill_start_lag_all_pools
 
 Operation = Callable[[str, list[str]], None]
 
